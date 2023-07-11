2519df1bca297488406a2622a1244ac08d5a28fe
John Doe <johndoe@email.com>
2023-07-12 00:47:46 +0530

    hi

--- src/Componants/Navbarr.js
+++ src/Componants/Navbarr.js
@@ -2,8 +2,18 @@ import React from "react"
 
 const Navbarr=()=>{
 return(
-    <div>
+    <div className="navbar">
         <h1>Jagruti Burde</h1>
+        <div className="menu">
+            <a href="#">Home</a>
+            <a href="#">About Us</a>
+            <a href="#">Contact Us
+            </a>
+            <a href="#">Skills
+            </a>
+            <a href="#">Qualification
+            </a>
+        </div>
     </div>
 )
 
